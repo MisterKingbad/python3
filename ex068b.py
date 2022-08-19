e787c86839510f5fe47027586f4782d237292f19
MisterKingBad <davymichael20@gmail.com>
2022-08-19 17:00:49 -0300

    [UPD] Novos exercicios

--- ex068b.py
+++ ex068b.py
@@ -1,5 +1,8 @@
 from random import randint
 while True:
-valor = int(input('digite um valor: '))
-comp = randint(0,10)
-tipo = ' '
+    valor = int(input('digite um valor: '))
+    comp = randint(0,10)
+    tot = valor + comp
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('PAR OU IMPAR? [P/I]'))
